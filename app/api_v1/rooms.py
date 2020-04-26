40bedbdd42d7403185e8d46b8fa8f7f39ff1dffd
Stareven233 <1429960342@qq.com>
2020-04-26 23:34:28 +0800

    用户头像处理

--- app/api_v1/rooms.py
+++ app/api_v1/rooms.py
@@ -5,7 +5,7 @@ from .. import redis_db, socketio
 from secrets import token_urlsafe
 from flask_socketio import disconnect, rooms
 from ..exceptions import NotRoomError, NotConnectError, UpTypeError
-from config import STATICS_DEST, MSG_PER_PAGE, ALLOWED_IMG_EXT
+from config import STATICS_DEST, MSG_PER_PAGE, ALLOWED_IMG_EXT, DEFAULT_AVATAR
 from json import loads
 from flask import url_for
 from os import remove
@@ -13,22 +13,27 @@ from os import remove
 get_participants = socketio.server.manager.get_participants
 
 
+def store_avatar(img, key):
+    m_type = img.mimetype.split('/')
+    if m_type[0] != 'image' or m_type[1] not in ALLOWED_IMG_EXT:
+        raise UpTypeError('检查图片后缀是否存在且合法')
+    # 图片靠nginx代理，redis保存文件名仅供更新删除
+
+    filename = redis_db.hget(key, 'avatar')
+    if filename and filename != DEFAULT_AVATAR:
+        remove(STATICS_DEST + f'\\img\\{filename}')
+
+    filename = f'{key}.{m_type[1]}'
+    img.save(STATICS_DEST + f'\\img\\{filename}')
+    return filename
+
+
 def store_info(args, room_id):
     avatar = args['avatar']
     room_key = f'room_{room_id}'
 
     if avatar is not None:
-        m_type = avatar.mimetype.split('/')
-        if m_type[0] != 'image' or m_type[1] not in ALLOWED_IMG_EXT:
-            raise UpTypeError('检查图片后缀是否存在且合法')
-        # 图片靠nginx代理，redis保存文件名仅供更新删除
-
-        filename = redis_db.hget(room_key, 'avatar')
-        if filename:
-            remove(STATICS_DEST+f'\\img\\{filename}')
-        filename = f'{room_key}.{m_type[1]}'
-        avatar.save(STATICS_DEST+f'\\img\\{filename}')
-        args['avatar'] = filename
+        args['avatar'] = store_avatar(avatar, room_key)
 
     room_info = {k: v for k, v in args.items() if v is not None}  # redis不允许None
     redis_db.hmset(room_key, room_info)
@@ -97,7 +102,7 @@ class RoomAPI(Resource):
         room_id = args["room"]
 
         room_info = redis_db.hgetall(f'room_{room_id}')
-        filename = room_info.get('avatar', '') or 'default.png'
+        filename = room_info.get('avatar', '') or DEFAULT_AVATAR
         room_info['avatar'] = url_for('main.room_avatar', filename=filename)
 
         response = {'code': 0, 'msg': "", 'data': room_info}
@@ -114,17 +119,44 @@ class MsgHistoryAPI(Resource):
     def get(self):
         args = self.reqparse.parse_args(strict=True)
         room_id = args['room']
-        room_key = f'msg_{room_id}'
+        msg_key = f'msg_{room_id}'
 
         pn = 0 if args['page'] < 1 else args['page']-1  # 将其限制到正整数再-1
         pn = pn*MSG_PER_PAGE + 1  # 最终将正整数页码转换为消息list的负索引
-        pn = -min(pn, redis_db.llen(room_key)+1)  # 限制pn上界，pn=len+1表示pn>len，查询为空
+        pn = -min(pn, redis_db.llen(msg_key)+1)  # 限制pn上界，pn=len+1表示pn>len，查询为空
 
-        msg_list = redis_db.lrange(room_key, pn-MSG_PER_PAGE+1, pn)
+        msg_list = redis_db.lrange(msg_key, pn-MSG_PER_PAGE+1, pn)
         msg_list = list(map(lambda x: loads(x), msg_list))
         response = {'code': 0, 'msg': "", 'data': msg_list[::-1]}
         return response, 200
 
 
+class UserAvatarAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('sid', type=str, required=True, location=['json', 'form'])
+        super().__init__()
+
+    def get(self):
+        args = self.reqparse.parse_args(strict=True)
+        user_key = f"user_{args['sid']}"
+
+        filename = redis_db.hget(user_key, 'avatar') or DEFAULT_AVATAR
+        response = {'code': 0, 'msg': "", 'data': url_for('main.room_avatar', filename=filename)}
+        return response, 200
+
+    def put(self):
+        self.reqparse.add_argument('avatar', type=FileStorage, required=True, location='files')
+        args = self.reqparse.parse_args(strict=True)
+
+        user_key = f"user_{args['sid']}"
+        filename = store_avatar(args['avatar'], user_key)
+        redis_db.hset(user_key, 'avatar', filename)
+
+        response = {'code': 0, 'msg': ""}
+        return response, 200
+
+
 api.add_resource(RoomAPI, '/rooms', endpoint='rooms')
 api.add_resource(MsgHistoryAPI, '/messages', endpoint='messages')
+api.add_resource(UserAvatarAPI, '/users/avatar', endpoint='avatar')
--- app/main/sockets.py
+++ app/main/sockets.py
@@ -1,5 +1,5 @@
 from .. import socketio, redis_db, compact_dumps
-from flask_socketio import emit, join_room, leave_room, disconnect, rooms
+from flask_socketio import emit, join_room, leave_room, disconnect, rooms, Namespace
 from flask import request
 from config import STATICS_DEST
 from os import remove
@@ -10,77 +10,66 @@ r_members = socketio.server.manager.rooms
 # r_members[namespace][room_id]: 获取房间成员，可用于计算长度，字典类型，不可在迭代中删除键
 
 
-@socketio.on('connect', namespace='/chat')
-def on_connect():
-    print('连接上了', request.sid)
+class ChatRoom(Namespace):
+    def __init__(self, namespace):
+        super().__init__(namespace)
+
+    def on_connect(self):
+        emit('sid', request.sid, room=request.sid)
+        print('连接上了', request.sid)
+
+    def on_disconnect(self):
+        user_key = f'user_{request.sid}'
+        room_id = redis_db.hget(user_key, 'room')
+        room_key = f'room_{room_id}'  # 'room_None' if room_id is None
+
+        if room_id in rooms():  # 最后1人断开时socket会删去记录，此if为False
+            for p in get_participants('/chat', room_id):
+                disconnect(p)  # 将该用户创建房间中其他人踢出
+
+        for k in (user_key, room_key):
+            avatar = redis_db.hget(k, 'avatar')
+            if not avatar:
+                continue
+            remove(STATICS_DEST + f'/img/{avatar}')
+
+        redis_db.delete(user_key, room_key, f'msg_{room_id}')
+        print('关闭了连接', request.sid)
+
+    def on_join(self, data):
+        room_id = data.pop('room', '')
+        if not redis_db.exists(f'room_{room_id}'):
+            return
+        join_room(room_id)  # 房主自己也得join
+        print('有人加入了？')
+        emit('online_delta', 1, broadcast=True, room=room_id)
+        emit('response', data['uname'] + '加入了房间', broadcast=True, room=room_id)
+
+    def on_leave(self, data):
+        room_id = data.pop('room', '')
+        if room_id not in rooms():
+            return
+        leave_room(room_id)
+        print('有人离开了？')
+        emit('online_delta', -1, broadcast=True, room=room_id)
+        emit('response', data['uname'] + '离开了房间', broadcast=True, room=room_id)
+
+    def on_chat(self, data):
+        room_id = data.pop('room', '')
+        if room_id not in rooms() or not data.get('msg', ''):
+            return
+        redis_db.rpush(f'msg_{room_id}', compact_dumps(data))
+        print('有人在聊天！')
+        emit('chat', data, broadcast=True, room=room_id)  # , include_self=False
+
+    def on_online_cnt(self, data):
+        room_id = data['room']
+        if room_id not in rooms():
+            emit('online_count', 0, room=room_id)
+            return
+        num = len(r_members['/chat'][room_id].keys())
+        print('是谁再问人数', num)
+        emit('online_count', num, room=room_id)
 
 
-@socketio.on('disconnect', namespace='/chat')
-def on_disconnect():
-    room_id = redis_db.hget(f'user_{request.sid}', 'room')
-    if room_id is None:
-        return
-    room_key = f'room_{room_id}'
-
-    if room_id in rooms():  # 最后1人断开时socket会删去记录，此if为False
-        for p in get_participants('/chat', room_id):
-            disconnect(p)  # 将该用户创建房间中其他人踢出
-
-    avatar = redis_db.hget(room_key, 'avatar')
-    if avatar:
-        remove(STATICS_DEST + f'\\img\\{avatar}')
-    redis_db.delete(f'user_{request.sid}', room_key, f'msg_{room_id}')
-    print('关闭了连接', request.sid)
-
-
-@socketio.on('join', namespace='/chat')
-def on_join(data):
-    room_id = data.pop('room', '')
-    if not redis_db.exists(f'room_{room_id}'):
-        return
-    join_room(room_id)  # 房主自己也得join
-    print('有人加入了？')
-    emit('online_delta', 1, broadcast=True, room=room_id)
-    emit('response', data['uname'] + '加入了房间', broadcast=True, room=room_id)
-
-
-@socketio.on('leave', namespace='/chat')
-def on_leave(data):
-    room_id = data.pop('room', '')
-    if room_id not in rooms():
-        return
-    leave_room(room_id)
-    print('有人离开了？')
-    emit('online_delta', -1, broadcast=True, room=room_id)
-    emit('response', data['uname'] + '离开了房间', broadcast=True, room=room_id)
-
-
-@socketio.on('chat', namespace='/chat')
-def on_chatting(data):
-    room_id = data.pop('room', '')
-    if room_id not in rooms() or not data.get('msg', ''):
-        return
-    redis_db.rpush(f'msg_{room_id}', compact_dumps(data))
-    print('有人在聊天！')
-    emit('chat', data, broadcast=True, room=room_id)  # , include_self=False
-
-
-@socketio.on('online_cnt', namespace='/chat')
-def online_count(data):
-    room_id = data['room']
-    if room_id not in rooms():
-        emit('online_count', 0, room=room_id)
-        return
-    num = len(r_members['/chat'][room_id].keys())
-    print('是谁再问人数', num)
-    emit('online_count', num, room=room_id)
-
-
-@socketio.on('test', namespace='/chat')
-def on_test():
-    return
-    # raise NotRoomError()
-    # print(rooms(data['sid']))
-    # members = list(get_participants('/chat', data['sid']))
-    # info = json.dumps(members, **json_config)
-    # print(len(members), info)
+socketio.on_namespace(ChatRoom('/chat'))
